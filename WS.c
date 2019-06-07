49302f096beb910878d0e2cd287ffec9c742f483
Klaoude <neoklaoude@gmail.com>
2019-06-07 10:16:32 +0200

    unittest

--- WS.c
+++ WS.c
@@ -75,7 +75,7 @@ int callbackOgar(struct lws *wsi, enum lws_callback_reasons reason, void *user, 
 	{
 	case LWS_CALLBACK_CLIENT_ESTABLISHED:
 		fprintf(stderr, "ogar: LWS_CALLBACK_CLIENT_ESTABLISHED\n");
-
+		
         unsigned char connect[5] = {0xff, 0, 0, 0, 0};
 		sendCommand(wsi, connect, 5);
 
--- unitTest.c
+++ unitTest.c
@@ -1,18 +1,40 @@
 #include "Utils.h"
 
+Node* createNode(int id, char* name)
+{
+    Node* node;
+    node->nodeID = 1;
+    node->x = 1337;
+    node->y = 420;
+    node->size = 42;
+    node->R = 255;
+    node->G = 0;
+    node->B = 0;
+    node->flags = 0x8;
+    node->name = malloc(strlen(name) + 1);
+    strcpy(node->name, name);
+
+    return node;
+}
+
+void printNodeStack(NodeStack* ns)
+{
+    NodeStack* tmp = ns;
+    if(tmp == NULL)
+        printf("Empty")
+;    while(tmp != NULL)
+    {
+        printf("[%d, %s] -> ", tmp->node->nodeID, tmp->node->name);
+        tmp = tmp->next;
+    }
+    printf("\n");
+}
+
 int main(int argc, char** argv)
 {
     NodeStack* ns;
-    Node node;
-    node.nodeID = 1;
-    node.x = 1337;
-    node.y = 420;
-    node.size = 42;
-    node.R = 255;
-    node.G = 0;
-    node.B = 0;
-    node.flags = 0x8;
-    node.name = malloc(5);
-    strcpy(node.name, "toto");
-    NodeStack_push(&ns, &node);
+    NodeStack_push(&ns, createNode(1, "bot1"));
+    NodeStack_push(&ns, createNode(2, "bot2"));
+    NodeStack_push(&ns, createNode(3, "bot3"));
+    printNodeStack(ns);
 }
