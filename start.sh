78d9ea9b9c070e73c7bbb95508272660870a417f
klaoude <neoklaoude@gmail.com>
2019-06-17 18:33:39 +0200

    Revert "Code plus joli"
    This reverts commit 9237dc5b6309e1d74fa4a967efac333a51ecd6a4.

--- start.sh
+++ start.sh
@@ -4,10 +4,9 @@ make
 
 ./AgoBot -g 127.0.0.1&
 ./AgoBot -n blue 127.0.0.1&
-./AgoBot -n blue 127.0.0.1&
-#./AgoBot -n purple 127.0.0.1&
-#./AgoBot -n purple 127.0.0.1&
 ./AgoBot -n purple 127.0.0.1&
 ./AgoBot -n purple 127.0.0.1&
-./AgoBot -g -n purple 127.0.0.1&
-./AgoBot -g -n purple 127.0.0.1&
+./AgoBot -n purple 127.0.0.1&
+./AgoBot -n purple 127.0.0.1&
+#./AgoBot -g -n purple 127.0.0.1&
+#./AgoBot -g -n purple 127.0.0.1&
