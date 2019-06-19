f41773fee7d0cfe983a11f862cd310785a5eb380
klaoude <neoklaoude@gmail.com>
2019-06-19 12:34:46 +0200

    Berger rework

--- start.sh
+++ start.sh
@@ -4,9 +4,10 @@ make
 
 ./AgoBot -g 127.0.0.1&
 ./AgoBot -n blue 127.0.0.1&
-./AgoBot -n purple 127.0.0.1&
-./AgoBot -n purple 127.0.0.1&
-./AgoBot -n purple 127.0.0.1&
-./AgoBot -n purple 127.0.0.1&
+./AgoBot -n blue 127.0.0.1&
+./AgoBot -g -n purple 127.0.0.1&
+./AgoBot -g -n purple 127.0.0.1&
+./AgoBot -g -n purple 127.0.0.1&
+./AgoBot -g -n purple 127.0.0.1&
 #./AgoBot -g -n purple 127.0.0.1&
 #./AgoBot -g -n purple 127.0.0.1&
